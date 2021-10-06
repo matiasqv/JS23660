115a520a2c0df22f31f0e42a8461e4f0101c8580
matiasqv <matiasqv@gmail.com>
2021-10-06 09:38:43 -0300

    clase 02

---
+++ js/miarchivo.js
@@ -0,0 +1,15 @@
+
+
+
+// Clase 02 Desafío: Crear un algoritmo con un condicional
+
+let numero =parseInt(prompt("Ingresar un numero entero"));
+if (numero % 2 == 0) {
+ alert("El numero ingresado es par");
+}
+else if ((numero + 1) % 2 == 0) {
+    alert("El numero ingresado es impar");
+   }
+else {
+ alert("No ingresaste un numero");
+}
