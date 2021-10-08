1853ec5c82610a06b6f17e4b71162f64b1672b9d
matiasqv <matiasqv@gmail.com>
2021-10-08 06:49:01 -0300

    clase 03

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,5 +1,5 @@
 
-// Clase 02 Desafío: Crear un algoritmo con un condicional
+/* // Clase 02 Desafío: Crear un algoritmo con un condicional
 
 let numero =parseFloat(prompt("Ingresar un numero entero"));
 if (numero % 2 == 0) {
@@ -13,4 +13,18 @@ else if ((numero % 2 > 0) && (numero % 2 < 1)) {
 }
 else {
     alert("No ingresaste un numero");
-}
+} */
+
+// Clase 03 Desafío: Crear un algoritmo utilizando un ciclo
+
+// Clacula el area y perimetro de un cuadrado rectangulo y saliendo por consola, tantas veces como se quiera
+
+let lado = parseFloat(prompt("Ingrese el lado del cuadrado en cm.\nPasa salir ingrese 0 "));
+let area = (lado * lado);
+let perimetro = (lado * 4);
+while ( lado !=  0) {
+    console.log ("El area del cuadrado es: "+area+" cm²"+"\nY su perimetro es: "+perimetro+" cm²" );
+    lado = parseFloat(prompt("Ingrese el lado del cuadrado en cm.\nPasa salir ingrese 0 "));
+    area = (lado * lado);
+    perimetro = (lado * 4);
+};
