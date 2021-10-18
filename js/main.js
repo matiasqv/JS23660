fdd48bd417de28fe0ee3e97703d455024a35b955
matiasqv <matiasqv@gmail.com>
2021-10-18 17:35:18 -0300

    clase 05

--- js/main.js
+++ js/main.js
@@ -1,222 +1,140 @@
+// Clase 05 Desafío: Incorporar objetos
 
-/* // Clase 02 Desafío: Crear un algoritmo con un condicional
-
-let numero =parseFloat(prompt("Ingresar un numero entero"));
-if (numero % 2 == 0) {
-    alert("El numero ingresado es par");
-}
-else if ((numero + 1) % 2 == 0) {
-    alert("El numero ingresado es impar");
-}
-else if ((numero % 2 > 0) && (numero % 2 < 1)) {
-    alert("El numero ingresado es un numero decimal");
-}
-else {
-    alert("No ingresaste un numero");
-} */
-
-
-/* // Clase 03 Desafío: Crear un algoritmo utilizando un ciclo
-
-// Clacula el area y perimetro de un cuadrado rectangulo y saliendo por consola, tantas veces como se quiera
-
-let lado = parseFloat(prompt("Ingrese el lado del cuadrado en cm.\nPasa salir ingrese 0 "));
-let area = (lado * lado);
-let perimetro = (lado * 4);
-while ( lado !=  0) {
-    console.log ("El area del cuadrado es: "+area+" cm²"+"\nY su perimetro es: "+perimetro+" cm" );
-    lado = parseFloat(prompt("Ingrese el lado del cuadrado en cm.\nPasa salir ingrese 0 "));
-    area = (lado * lado);
-    perimetro = (lado * 4);
-}; */
-
-
-/* // Clase 04 Desafío: Simulador interactivo
-
-
-let cuenta = 0;
-let total = 0;
 let comida = 0;
-let bebida = 0;
-let cantidadBebida = 0;
 let cantidadComida = 0;
 let precioComida = 0;
-let precioBebida =0;
-let propina = 0;
-let porcentajePropina = 0;
-let iva = 0;
-let descuento =0;
+let bebida = 0;
+let cantidadBebida = 0;
+let precioBebida = 0;
 
-function pedirComida() {
+class Pedido {
+        constructor(comida, precioComida, cantidadComida, bebida, precioBebida, cantidadBebida){
+            this.comida = comida;
+            this.cantidadComida = cantidadComida;
+            this.precioComida = precioComida;
+            this.bebida = bebida;
+            this.cantidadBebida = cantidadBebida;
+            this.precioBebida = precioBebida;
+            this.propina = 0;
+            this.porcentajePropina = 0;
+            this.iva = 0;
+            this.descuento = 0;
+            this.cuenta = 0;
+            this.total = 0;
+        }
 
-    while(isNaN(comida) || comida == 0 || comida > 4 || comida < 0) {
-        comida = parseInt(prompt(
-            "¿Qué va a comer? Introduce el número junto a la opción:\n 1:Pancho($100),\n 2:Hamburguesa($350),\n 3:Picada($700),\n 4:Tostados($200)"));
+    calcularCuenta(){
+        this.cuenta = (this.cantidadComida*this.precioComida) + (this.cantidadBebida*this.precioBebida)
     }
 
-    while(isNaN(cantidadComida) || cantidadComida == 0 || cantidadComida < 0) {
-        cantidadComida = parseInt(prompt("Introduce la cantidad deseada.(Solo números)"));
+    calcularIva(){
+    this.iva = this.cuenta * 0.21;
+    }
+
+    aplicarDescuento() {
+        if(this.cuenta >= 2500) {
+            this.descuento = this.cuenta * -0.1;
+            this.total = this.cuenta + this.descuento;
+        }
+        return this.total;
+    }
+    
+    darPropina() {
+        this.propina = confirm("¿Te Gustaría dejar propina?");
+        
+        if(this.propina) { 
+            while(!this.porcentajePropina  || this.porcentajePropina <= 0)
+            {
+                this.porcentajePropina = parseFloat(prompt("¿QUE %? (Solo números)"));
+            }
+            this.propina = this.cuenta * this.porcentajePropina /100;
+            this.total = this.total + this.propina;      
+        } else {
+            this.propina = 0;
+        }
+    }
+}
+
+function tomarPedido() {
+    while(!comida || comida == 0 || comida > 4 || comida < 0) {
+        comida = parseInt(prompt(
+            "¿Qué va a comer? Introduce el número junto a la opción:\n 1:Pancho($100),\n 2:Hamburguesa($350),\n 3:Picada($700),\n 4:Tostados($200)"));
     }
 
     switch (comida) {
         case 1 :
             comida = "Pancho ($100)";
             precioComida = 100;
-            cuenta += (precioComida * cantidadComida);
             break;
         case 2 :
             comida = "Hamburguesa ($350)";
             precioComida = 350;
-            cuenta += (precioComida * cantidadComida);
             break;
         case 3 :
             comida = "Picada ($700)";
-            precioComida = 700;
-            cuenta += (precioComida * cantidadComida);
+            precioComida = 700;   
             break;
         case 4 :
             comida = "Tostados($200)";
             precioComida = 200;
-            cuenta += (precioComida * cantidadComida);
             break;
     }
-}
 
-function pedirBebida() {
+    while(!cantidadComida || cantidadComida == 0 || cantidadComida < 0) {
+        cantidadComida = parseInt(prompt("Introduce la cantidad deseada.(Solo números)"));
+    }
 
-    while(isNaN(bebida) || bebida == 0 || bebida > 4 || bebida < 0) {
+    while(!bebida || bebida == 0 || bebida > 4 || bebida < 0) {
         bebida = parseInt(prompt("¿Qué le gustaría beber? Introduce el número junto a la opción:\n 1:Gaseosa($150),\n 2:Agua($100),\n 3:Jugo($200),\n 4:Agua gasificada($120)"));
     }
 
-    while(isNaN(cantidadBebida) || cantidadBebida == 0 || cantidadBebida < 0) {
+    switch (bebida) {
+        case 1 :
+            bebida = "Gaseosa ($150)";
+            precioBebida = 150;
+            break;
+        case 2 :
+            bebida = "Agua ($100)";
+            precioBebida = 100;
+            break;
+        case 3 :
+            bebida = "Jugo ($200))";
+            precioBebida = 200
+            break;
+        case 4 :
+            bebida = "Agua gasificada ($120)";
+            precioBebida = 120;
+            break;
+    }
+
+    while(!cantidadBebida || cantidadBebida == 0 || cantidadBebida < 0) {
         cantidadBebida = parseInt(prompt("Introduce la cantidad deseada.(Solo números)"));
     }
 
-    if(cantidadBebida) {
-        switch (bebida) {
-            case 1 :
-                bebida = "Gaseosa ($150)";
-                precioBebida = 150;
-                cuenta += (precioBebida * cantidadBebida);
-                break;
-            case 2 :
-                bebida = "Agua ($100)";
-                precioBebida = 100;
-                cuenta += (precioBebida * cantidadBebida);
-                break;
-            case 3 :
-                bebida = "Jugo ($200))";
-                precioBebida = 200;
-                cuenta += (precioBebida * cantidadBebida);
-                break;
-            case 4 :
-                bebida = "Agua gasificada ($120)";
-                precioBebida = 120;
-                cuenta += (precioBebida * cantidadBebida);
-                break;
-        }
-    }
-}
+    return new Pedido(comida, precioComida, cantidadComida, bebida, precioBebida, cantidadBebida)
 
-function calcularIva(){
-    iva = cuenta * 0.21;
-}
-
-function aplicarDescuento() {
-    if(cuenta >= 2500) {
-        descuento = cuenta * -0.1;
-        total = cuenta + descuento;
-    }
-    return total;
-}
-
-
-function darPropina() {
-    propina = confirm("¿Te Gustaría dejar propina?");
-    
-    if(propina) { 
-        while(isNaN(porcentajePropina)  || porcentajePropina <= 0)
-        {
-            porcentajePropina = parseFloat(prompt("¿QUE %? (Solo números)"));
-        }
-        propina = cuenta * porcentajePropina /100;
-        total = total + propina;      
-    } else {
-        propina = 0;
-    }
 }
 
 
 alert("Ingrese su pedido");
 
-pedirComida();
-pedirBebida();
-calcularIva();
-aplicarDescuento();
-darPropina();
+const pedido = new tomarPedido();
+pedido.calcularCuenta();
+pedido.calcularIva();
+pedido.aplicarDescuento();
+pedido.darPropina();
 
 alert(`
     Detalle de la cuenta:
-    - ${cantidadComida} x ${comida} = $${cantidadComida*precioComida}
-    - ${cantidadBebida} x ${bebida} = $${cantidadBebida*precioBebida}
+    - ${pedido.cantidadComida} x ${pedido.comida} = $${pedido.cantidadComida*pedido.precioComida}
+    - ${pedido.cantidadBebida} x ${pedido.bebida} = $${pedido.cantidadBebida*pedido.precioBebida}
     
-    SubTotal = $${cuenta} (iva = $${iva})
-    -Descuento = $${descuento}
-    -Propina = $${propina} (% = ${porcentajePropina})
+    SubTotal = $${pedido.cuenta} (iva = $${pedido.iva})
+    -Descuento = $${pedido.descuento}
+    -Propina = $${pedido.propina} (% = ${pedido.porcentajePropina})
     ==========================
-    Total = $${total}
-`); */
-
-
-
-
-// Clase 04 Desafío: Simulador interactivo
-
-
-
-const multiplicar = (a,b) => a * b;
-const suma = (a,b) => a + b;
-const resta = (a,b) => a - b;
-const porcentaje = (a,b) => multiplicar (a,b)/100;
-const iva = x => x *0.21;
-
-
-
-
-let comida =  "Pancho ($100)";
-let precioComida = 100;
-let cantidadComida = 4;
-let bebida = "Gaseosa ($150)";
-let precioBebida = 150;
-let cantidadBebida = 4;
-let totalDesc = 10;
-let porcentajePropina = 15;
-
-
-alert(`
-    Detalle de la cuenta:
-    - ${cantidadComida} x ${comida} = $${multiplicar(cantidadComida,precioComida)}
-    - ${cantidadBebida} x ${bebida} = $${multiplicar(cantidadBebida,precioBebida)}
-    
-    SubTotal = $${suma(multiplicar(cantidadComida,precioComida),multiplicar(cantidadBebida,precioBebida))} (iva = $${iva(suma(multiplicar(cantidadComida,precioComida),multiplicar(cantidadBebida,precioBebida)))})
-    -Descuento = $${porcentaje(suma(multiplicar(cantidadComida,precioComida),multiplicar(cantidadBebida,precioBebida)),totalDesc)}
-    -Propina = $${porcentaje(suma(multiplicar(cantidadComida,precioComida),multiplicar(cantidadBebida,precioBebida)),porcentajePropina)} (% = ${porcentajePropina})
-    ==========================
-    Total = $${suma(suma(multiplicar(cantidadComida,precioComida),multiplicar(cantidadBebida,precioBebida)),
-                resta(porcentaje(suma(multiplicar(cantidadComida,precioComida),multiplicar(cantidadBebida,precioBebida)),porcentajePropina),
-                    porcentaje(suma(multiplicar(cantidadComida,precioComida),multiplicar(cantidadBebida,precioBebida)),totalDesc)))}
+    Total = $${pedido.total}
 `);
-
-console.log("Total: $"+suma(suma(multiplicar(cantidadComida,precioComida),multiplicar(cantidadBebida,precioBebida)),
-            resta(porcentaje(suma(multiplicar(cantidadComida,precioComida),multiplicar(cantidadBebida,precioBebida)),porcentajePropina),
-                porcentaje(suma(multiplicar(cantidadComida,precioComida),multiplicar(cantidadBebida,precioBebida)),totalDesc))));
-
-
-
-
-
-
-
 
 
 
