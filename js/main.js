f2544fa1aef45bad1c0fd75036c8ede1b2f79701
matiasqv <matiasqv@gmail.com>
2021-10-20 19:37:00 -0300

    clase 06 complementario

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Clase 06 Desafío: Incorporar Arrays
+// // Clase 06 Desafío Complementario: Ordenar un array de objetos
 
 let comida = 0;
 let cantidadComida = 0;
@@ -8,49 +8,48 @@ let cantidadBebida = 0;
 let precioBebida = 0;
 
 class Pedido {
-        constructor(comida, precioComida, cantidadComida, bebida, precioBebida, cantidadBebida){
-            this.comida = comida;
-            this.cantidadComida = cantidadComida;
-            this.precioComida = precioComida;
-            this.bebida = bebida;
-            this.cantidadBebida = cantidadBebida;
-            this.precioBebida = precioBebida;
-            this.propina = 0;
-            this.porcentajePropina = 0;
-            this.iva = 0;
-            this.descuento = 0;
-            this.cuenta = 0;
-            this.total = 0;
-        }
-
-    calcularCuenta(){
-        this.cuenta = (this.cantidadComida*this.precioComida) + (this.cantidadBebida*this.precioBebida)
+    constructor(comida, precioComida, cantidadComida, bebida, precioBebida, cantidadBebida) {
+        this.comida = comida;
+        this.cantidadComida = cantidadComida;
+        this.precioComida = precioComida;
+        this.bebida = bebida;
+        this.cantidadBebida = cantidadBebida;
+        this.precioBebida = precioBebida;
+        this.propina = 0;
+        this.porcentajePropina = 0;
+        this.iva = 0;
+        this.descuento = 0;
+        this.cuenta = 0;
+        this.total = 0;
     }
 
-    calcularIva(){
-    this.iva = this.cuenta * 0.21;
+    calcularCuenta() {
+        this.cuenta = (this.cantidadComida * this.precioComida) + (this.cantidadBebida * this.precioBebida)
+    }
+
+    calcularIva() {
+        this.iva = this.cuenta * 0.21;
     }
 
     aplicarDescuento() {
-        if(this.cuenta >= 2500) {
+        if (this.cuenta >= 2500) {
             this.descuento = this.cuenta * -0.1;
             this.total = this.cuenta + this.descuento;
         }
-        else{
+        else {
             this.total = this.cuenta;
         }
     }
 
     darPropina() {
         this.propina = confirm("¿Te Gustaría dejar propina?");
-        
-        if(this.propina) { 
-            while(!this.porcentajePropina  || this.porcentajePropina <= 0)
-            {
+
+        if (this.propina) {
+            while (!this.porcentajePropina || this.porcentajePropina <= 0) {
                 this.porcentajePropina = parseFloat(prompt("¿QUE %? (Solo números)"));
             }
-            this.propina = this.cuenta * this.porcentajePropina /100;
-            this.total = this.total + this.propina;   
+            this.propina = this.cuenta * this.porcentajePropina / 100;
+            this.total = this.total + this.propina;
         } else {
             this.propina = 0;
         }
@@ -61,16 +60,16 @@ class Pedido {
 function tomarPedido() {
 
     const pedidoComida = [
-        {id:1, comida: 'Pancho', precioComida: 100},
-        {id:2, comida: 'Hamburguesa', precioComida: 350},
-        {id:3, comida: 'Picada', precioComida: 700},
-        {id:4, comida: 'Tostados', precioComida: 200},
+        { id: 1, comida: 'Pancho', precioComida: 100 },
+        { id: 2, comida: 'Hamburguesa', precioComida: 350 },
+        { id: 3, comida: 'Picada', precioComida: 700 },
+        { id: 4, comida: 'Tostados', precioComida: 200 },
     ];
 
-    while(!comida || comida == 0 || comida > 5 || comida < 0) {
+    while (!comida || comida == 0 || comida > 5 || comida < 0) {
         comida = parseInt(prompt(
             "¿Qué va a comer? Introduce el número junto a la opción:\n 1: Pancho($100),\n 2: Hamburguesa($350),\n 3: Picada($700),\n 4: Tostados($200),\n 5: Otro comida"));
-            
+
         if (comida == 1) {
             comida = pedidoComida[0].comida;
             precioComida = pedidoComida[0].precioComida;
@@ -88,31 +87,43 @@ function tomarPedido() {
             precioComida = pedidoComida[3].precioComida;
         }
         else if (comida == 5) {
-            let entradaComida = prompt ("Ingresar otra comida");
-            let entradaPrecioComida = prompt ("ingresa precio de otra comida");
-            pedidoComida.push({id: (comida), comida:(entradaComida), precioComida: (entradaPrecioComida)});
+            let entradaComida = prompt("Ingresar otra comida");
+            let entradaPrecioComida;
+            while (!entradaPrecioComida || entradaPrecioComida == 0 || entradaPrecioComida < 0) {
+                entradaPrecioComida = parseInt(prompt("ingresa precio de otra comida"));
+            }
+            pedidoComida.push({ id: (comida), comida: (entradaComida), precioComida: (entradaPrecioComida) });
             comida = pedidoComida[4].comida;
             precioComida = pedidoComida[4].precioComida;
         }
         else {
-            return new tomarPedido ;
+            comida = 0;
         }
     }
-    
-    while(!cantidadComida || cantidadComida == 0 || cantidadComida < 0) {
+
+    let ordenadosPrecioComida = [];
+    ordenadosPrecioComida = pedidoComida.map(elemento => elemento);
+    ordenadosPrecioComida.sort(function (a, b) {
+        return a.precioComida - b.precioComida;
+    });
+
+    console.log("Ordenar por precio comida");
+    console.log(ordenadosPrecioComida);
+
+    while (!cantidadComida || cantidadComida == 0 || cantidadComida < 0) {
         cantidadComida = parseInt(prompt("Introduce la cantidad deseada.(Solo números)"));
     }
 
     const pedidoBebida = [
-        {id:1, bebida: "Gaseosa", precioBebida: 150},
-        {id:2, bebida: "Agua", precioBebida: 100},
-        {id:3, bebida: "Jugo", precioBebida: 200},
-        {id:4, bebida: "Agua gasificada", precioBebida: 120},
+        { id: 1, bebida: "Gaseosa", precioBebida: 150 },
+        { id: 2, bebida: "Agua", precioBebida: 100 },
+        { id: 3, bebida: "Jugo", precioBebida: 200 },
+        { id: 4, bebida: "Agua gasificada", precioBebida: 120 },
     ]
 
-    while(!bebida || bebida == 0 || bebida > 5 || bebida < 0) {
+    while (!bebida || bebida == 0 || bebida > 5 || bebida < 0) {
         bebida = parseInt(prompt("¿Qué le gustaría beber? Introduce el número junto a la opción:\n 1: Gaseosa($150),\n 2: Agua($100),\n 3: Jugo($200),\n 4: Agua gasificada($120),\n 5: Otro bebida"));
-    
+
         if (bebida == 1) {
             bebida = pedidoBebida[0].bebida;
             precioBebida = pedidoBebida[0].precioBebida;
@@ -129,19 +140,31 @@ function tomarPedido() {
             bebida = pedidoBebida[3].bebida;
             precioBebida = pedidoBebida[3].precioBebida;
         }
-            else if (bebida == 5) {
-        let entradaBebida = prompt ("Ingresar otra bebida");
-        let entradaPrecioBebida = prompt ("Ingresa precio de otra bebida");
-        pedidoBebida.push({id: (bebida), bebida:(entradaBebida), precioBebida: (entradaPrecioBebida)});
-        bebida = pedidoBebida[4].bebida;
-        precioBebida = pedidoBebida[4].precioBebida;
+        else if (bebida == 5) {
+            let entradaBebida = prompt("Ingresar otra bebida");
+            let entradaPrecioBebida;
+            while (!entradaPrecioBebida || entradaPrecioBebida == 0 || entradaPrecioBebida < 0) {
+                entradaPrecioBebida = parseInt(prompt("Ingresa precio de otra bebida"));
+            }
+            pedidoBebida.push({ id: (bebida), bebida: (entradaBebida), precioBebida: (entradaPrecioBebida) });
+            bebida = pedidoBebida[4].bebida;
+            precioBebida = pedidoBebida[4].precioBebida;
         }
         else {
-            return new tomarPedido;
+            bebida = 0;
         }
     }
 
-    while(!cantidadBebida || cantidadBebida == 0 || cantidadBebida < 0) {
+    let ordenadosPrecioBebida = [];
+    ordenadosPrecioBebida = pedidoBebida.map(elemento => elemento);
+    ordenadosPrecioBebida.sort(function (a, b) {
+        return a.precioBebida - b.precioBebida;
+    });
+
+    console.log("Ordenar por precio bebida");
+    console.log(ordenadosPrecioBebida);
+
+    while (!cantidadBebida || cantidadBebida == 0 || cantidadBebida < 0) {
         cantidadBebida = parseInt(prompt("Introduce la cantidad deseada.(Solo números)"))
     }
 
@@ -158,8 +181,8 @@ pedido.darPropina();
 
 alert(`
     Detalle de la cuenta:
-    - ${pedido.cantidadComida} ${pedido.comida} x $${pedido.precioComida} = $${pedido.cantidadComida*pedido.precioComida}
-    - ${pedido.cantidadBebida} ${pedido.bebida} x $${pedido.precioBebida} = $${pedido.cantidadBebida*pedido.precioBebida}
+    - ${pedido.cantidadComida} ${pedido.comida} x $${pedido.precioComida} = $${pedido.cantidadComida * pedido.precioComida}
+    - ${pedido.cantidadBebida} ${pedido.bebida} x $${pedido.precioBebida} = $${pedido.cantidadBebida * pedido.precioBebida}
     
     SubTotal = $${pedido.cuenta} (iva = $${pedido.iva})
     -Descuento = $${pedido.descuento}
@@ -167,6 +190,120 @@ alert(`
     ==========================
     Total = $${pedido.total}
 `);
+
+
+// class Producto {
+//     constructor(nombre, precio, detalle, cantidad){
+//         this.nombre = nombre;
+//         this.precio = parseFloat(precio);
+//         this.detalle = detalle;
+//         this.cantidad = cantidad; 
+//         this.disponible = true;
+//     }
+//     sumarIva() {
+//         return this.precio * 1.21;
+//     }
+//     vender() {
+//         this.disponible = false;
+//     }
+//     precioSugerido() {
+//         return this.precio *1.21 *1.25;
+//     }
+// }
+
+// let arrayProductos = [];
+// do {
+//     let comprobacion = prompt("Ingrese nombre del producto o fin para terminar de agregar");
+//     console.log(comprobacion);
+//     if (comprobacion === "fin" || comprobacion === "FIM" || comprobacion === "Fin"){
+//         break;
+//     }else{
+//         nombreP = comprobacion;
+//         let precioP = prompt("Ingrese el precion del producto");
+//         let detalleP = prompt("Ingrese el detalle del producto");
+//         let cantidadP = prompt("Ingrese la cantidad comprada del producto");
+//         arrayProductos.push(new Producto(nombreP, precioP, detalleP, cantidadP));
+//         console.log(arrayProductos);
+//         console.log(comprobacion);
+//     }
+// }
+
+// while (nombreP != "fin" ||nombreP != "FIM" || nombreP != "Fin");
+// console.log(arrayProductos);
+
+// for (let producto of arrayProductos){
+//     console.log(producto.nombre);
+//     console.log(producto.detalle);
+//     console.log(producto.cantidad);
+//     console.log(producto.sumarIva());
+// }
+
+// // por stock
+
+// let pocoStock = arrayProductos.filter(producto => producto.cantidad <= 3);
+// console.log("poco stock");
+// console.log(pocoStock);
+
+// for (let producto of pocoStock){
+//     console.log(producto.nombre);
+//     console.log(producto.detalle);
+//     console.log(producto.cantidad);
+// }
+
+// // sin stock
+
+// let sinStock = arrayProductos.filter(producto => producto.cantidad = 0 || producto.disponible == false); 
+// console.log("sin stock");
+// console.log(sinStock);
+
+// for (let producto of sinStock){
+//     console.log(producto.nombre);
+//     console.log(producto.detalle);
+//     console.log(producto.cantidad);
+// }
+
+// ordenar por cantidad
+
+
+//     const pedidoComida = [
+//         {id:1, comida: 'Pancho', precioComida: 100},
+//         {id:2, comida: 'Hamburguesa', precioComida: 350},
+//         {id:3, comida: 'Picada', precioComida: 700},
+//         {id:4, comida: 'Tostados', precioComida: 200},
+//     ];
+
+
+
+// let ordenadosPrecio = [];
+// ordenadosPrecio = pedidoComida.map(elemento => elemento);
+// ordenadosPrecio.sort(function(a , b){
+//     return a.precioComida - b.precioComida;
+// });
+
+// console.log("Ordenar por precio");
+// console.log(ordenadosPrecio);
+
+// for (let pedidoComida of ordenadosPrecio){
+//     console.log(pedidoComida.id);
+//     console.log(pedidoComida.comida);
+//     console.log(pedidoComida.precioComida);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
