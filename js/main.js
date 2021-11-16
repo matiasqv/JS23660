cb49272377660fe7a3deefb2feb1225069ad0afd
matiasqv <matiasqv@gmail.com>
2021-11-15 21:49:33 -0300

    clase 13

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// // Clase 12 : Orden
+// // Clase 13 : Orden
 
 
 const misCuentas = [];
@@ -15,7 +15,7 @@ for (const producto of productos) {
     $(".lista-productos").prepend(`<div>
                             <h4> Producto: ${producto.bebida}</h4>
                             <b> $ ${producto.precioBebida}</b>
-                            <button id="btn${producto.id}">Comprar</button>
+                            <button class="comprar" id="btn${producto.id}">Comprar</button>
                             </div>`);
     for (let i = 0; i < 1; i++) {
         $(`#btn${producto.id}`).click(() => {
@@ -24,10 +24,21 @@ for (const producto of productos) {
                 bebida: (producto.bebida),
                 precioBebida: (producto.precioBebida),
             });
-            guardarCuentaLocalStorage()
-        })
-    }
+            guardarCuentaLocalStorage();
+        });
+    };
+    // Modifica el color del btn cuando pasa el mouse 
+    $(`#btn${producto.id}`).mouseover(function () { 
+        $(`#btn${producto.id}`).css({"background": "green",})
+                            .mouseout(function () { 
+                                $(`#btn${producto.id}`).css({"background": "none",})
+                            });
+    });
 };
+
+
+
+
 
 // STYLES  CSS
         $("h4").css({
@@ -45,6 +56,8 @@ for (const producto of productos) {
             width: '50%',
         });
 
+
+// Funcion Guardar LocalStorage
 function guardarCuentaLocalStorage() {
     localStorage.setItem('misCuentas', JSON.stringify(misCuentas));
     localStorage.setItem('Numero de productos agregados', misCuentas.length);
@@ -54,17 +67,15 @@ function guardarCuentaLocalStorage() {
 $("#btn-mostrar").prepend('<button id="btn-mostrar">Mostrar Pedido</button>');
 $("#btn-mostrar").click(() => {
     obtenerLocalStorage();
-
 });
 
-
+// Funcion Recuperar LocalStorage
 function obtenerLocalStorage() {
     let guardado = JSON.parse(localStorage.getItem('misCuentas'));
     $('#compra').append(`<h1>Resumen</h1>`);
     $("#compra").prepend('<button id="btn-cerrar">Cerrar</button>');
     $("#btn-cerrar").click(() => {
         location.reload();
-
     });
     $("#btn-mostrar").hide();
     for (const producto of guardado) {
@@ -77,6 +88,7 @@ function obtenerLocalStorage() {
 
 $("#btn-promo").append('<button id="btn-promo">Mostrar Promociones</button>');
 $("#promo").prepend(`<div id="promo">
+"HAS CLICK AQUI"<br>
 1: Pancho ($100)<br>
 2: Hamburguesa ($350)<br>
 3: Picada ($700)<br>
@@ -88,3 +100,24 @@ $("#promo").hide();
 $("#btn-promo").click(() => {
     $("#promo").toggle("slow");
 });
+
+
+
+// Animacion, hacer click en el div que muestra la promocion
+
+$("#promo").click(function (){
+    $("#btn-promo").fadeOut();
+    $("#promo").css("color","gray")
+                .slideUp(2000)
+                .delay(2000)
+                .slideDown(2000, function(){
+                    $("#btn-promo").fadeIn();
+                    $("#promo").css({"color":"black",
+                    "background": "green",
+                    "background-color": "green",
+                    "border-radius": "5%",
+                    "border": "5px black solid"
+                    });
+                });
+});
+
